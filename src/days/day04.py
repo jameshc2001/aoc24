53243afaf8765ae614d178d62eac51f28ed42c81
jameshc2001 <jameshcrofts@gmail.com>
2024-12-21 18:17:15 +0000

    day04

---
+++ src/days/day04.py
@@ -0,0 +1,81 @@
+import numpy as np
+
+def part01(input):
+    rows, cols, grid = create_grid(input)
+    total = 0
+    for x in range(cols):
+        for y in range(rows):
+            if (grid[x, y] != "X"): continue
+
+            if ((safe_get(grid, x, y) + safe_get(grid, x + 1, y) + safe_get(grid, x + 2, y) + safe_get(grid, x + 3, y)) == "XMAS"): total += 1
+            if ((safe_get(grid, x, y) + safe_get(grid, x - 1, y) + safe_get(grid, x - 2, y) + safe_get(grid, x - 3, y)) == "XMAS"): total += 1
+            if ((safe_get(grid, x, y) + safe_get(grid, x, y + 1) + safe_get(grid, x, y + 2) + safe_get(grid, x, y + 3)) == "XMAS"): total += 1
+            if ((safe_get(grid, x, y) + safe_get(grid, x, y - 1) + safe_get(grid, x, y - 2) + safe_get(grid, x, y - 3)) == "XMAS"): total += 1
+
+            if ((safe_get(grid, x, y) + safe_get(grid, x + 1, y + 1) + safe_get(grid, x + 2, y + 2) + safe_get(grid, x + 3, y + 3)) == "XMAS"): total += 1
+            if ((safe_get(grid, x, y) + safe_get(grid, x - 1, y - 1) + safe_get(grid, x - 2, y - 2) + safe_get(grid, x - 3, y - 3)) == "XMAS"): total += 1
+
+            if ((safe_get(grid, x, y) + safe_get(grid, x + 1, y - 1) + safe_get(grid, x + 2, y - 2) + safe_get(grid, x + 3, y - 3)) == "XMAS"): total += 1
+            if ((safe_get(grid, x, y) + safe_get(grid, x - 1, y + 1) + safe_get(grid, x - 2, y + 2) + safe_get(grid, x - 3, y + 3)) == "XMAS"): total += 1
+
+    return total
+
+def part02(input):
+    rows, cols, grid = create_grid(input)
+    permutations = ["SAM", "MAS"]
+    total = 0
+    for x in range(cols):
+        for y in range(rows):
+            if (grid[x, y] != "A"): continue
+
+            a = safe_get(grid, x - 1, y - 1) + "A" + safe_get(grid, x + 1, y + 1)
+            b = safe_get(grid, x + 1, y - 1) + "A" + safe_get(grid, x - 1, y + 1)
+            
+            if (a in permutations and b in permutations): total += 1
+    
+    return total
+
+def create_grid(input):
+    lines = input.split("\n")
+    rows = len(lines)
+    cols = len(lines[0])
+    grid = np.full((cols, rows), '', dtype=str)
+
+    for y, line in enumerate(lines):
+        for x, character in enumerate(line):
+            grid[x, y] = character
+
+    return rows,cols,grid
+
+def safe_get(grid, x, y):
+    a, b = grid.shape
+    if (0 <= x < a and 0 <= y < b): return grid[x, y]
+    return ""
+    
+
+#TESTS
+
+sample = """MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX"""
+
+def test_part01_sample():
+    assert 18 == part01(sample)
+
+def test_part01_input():
+    with open("src/inputs/day04.txt", "r") as f:
+        assert 2464 == part01(f.read())
+
+def test_part02_sample():
+    assert 9 == part02(sample)
+
+def test_part02_input():
+    with open("src/inputs/day04.txt", "r") as f:
+        assert 1982 == part02(f.read())
