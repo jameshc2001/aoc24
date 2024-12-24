b2e4a83a6b79df58a5232c207617fcd330764642
jameshc2001 <jameshcrofts@gmail.com>
2024-12-24 11:10:43 +0000

    day06 part02

--- src/days/day06.py
+++ src/days/day06.py
@@ -1,4 +1,7 @@
 import numpy as np
+import cProfile
+import line_profiler
+import os
 
 def part01(input):
     grid, max_y, max_x, guard, direction = initial_parameters(input)
@@ -13,11 +16,23 @@ def part02(input):
     for position in new_obstacle_positions:
         new_grid = np.array(grid)
         new_grid[position[1], position[0]] = '#'
-        if (has_loop(grid, max_y, max_x, guard, direction)): total += 1
+        if (has_loop(new_grid, max_y, max_x, guard, direction)): total += 1
 
     return total
 
+@line_profiler.profile
 def has_loop(grid, max_y, max_x, guard, direction):
+    path = {tuple([tuple(guard), direction])}
+    while (True):
+        next = np.add(guard, direction)
+        while(in_grid(max_y, max_x, next) and grid[next[1], next[0]] == '#'):
+            direction = rotate_90_degrees(direction)
+            next = np.add(guard, direction)
+        
+        guard = tuple(next)
+        if (tuple([guard, direction]) in path): return True
+        if (not in_grid(max_y, max_x, guard)): break
+        path.add(tuple([guard, direction]))
     return False
 
 
@@ -37,17 +52,17 @@ def get_path(grid, max_y, max_x, guard, direction):
             direction = rotate_90_degrees(direction)
             next = np.add(guard, direction)
         
-        guard = np.add(guard, direction)
+        guard = next
         if (not in_grid(max_y, max_x, guard)): break
         visited.add(tuple(guard))
     return visited
 
 def rotate_90_degrees(direction):
-    if (direction == (0, -1)): return (1, 0)
-    elif (direction == (0, 1)): return (-1, 0)
-    elif (direction == (-1, 0)): return (0, -1)
-    elif (direction == (1, 0)): return (0, 1)
-    return direction
+    match (direction):
+        case (0, -1): return (1, 0)
+        case (0, 1): return (-1, 0)
+        case (-1, 0): return (0, -1)
+        case (1, 0): return (0, 1)
 
 def in_grid(max_y, max_x, position):
     return 0 <= position[0] < max_x and 0 <= position[1] < max_y
@@ -86,3 +101,9 @@ def test_part01_input():
 
 def test_part02_sample():
     assert 6 == part02(sample)
+
+def test_part02_input():
+    with open("src/inputs/day06.txt", "r") as f:
+        assert 1604 == part02(f.read())
+
+#This runs much slower than expected. I think I'm going to avoid numpy for future days and keep the elements of my sets simple.
