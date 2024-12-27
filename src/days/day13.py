74f0c88ffcad16cfd755d6c73d2d36141df8a24c
jameshc2001 <jameshcrofts@gmail.com>
2024-12-27 17:24:02 +0000

    day13 part02

--- src/days/day13.py
+++ src/days/day13.py
@@ -1,36 +1,28 @@
 import re
+import pulp
 
-def part01(input):
+def solution(input, offset=0):
     tokens = 0
     for machine_text in input.split("\n\n"):
         lines = machine_text.split("\n")
-        tokens += min_tokens(
-            coords_for_line(lines[0]),
-            coords_for_line(lines[1]),
-            coords_for_line(lines[2])
-        )
+        a = coords_for_line(lines[0])
+        b = coords_for_line(lines[1])
+        p = coords_for_line(lines[2])
+        p = (p[0] + offset, p[1] + offset)
+        tokens += tokens_for_prize(a, b, p)
     return tokens
 
 def coords_for_line(line):
     numbers = re.findall(r"\d+", line)
     return (int(numbers[0]), int(numbers[1]))
 
-def min_tokens(a, b, p):
-    for B in reversed(range(100)):
-        A = (p[0] - (B * b[0])) / a[0]
-        if (not is_whole(A, 0.0000000001)): continue
-        if ((int(A) * a[0] + B * b[0]) != p[0]): continue
-        if ((int(A) * a[1] + B * b[1]) != p[1]): continue
-        return (int(A) * 3) + B
-    return 0
-
 def tokens_for_prize(a, b, p):
     b_pushes = (p[1] - ((a[1] * p[0]) / a[0])) / (b[1] - ((a[1] * b[0]) / a[0]))
     a_pushes = (p[0] - (b_pushes * b[0])) / a[0]
 
-    deviation = 0.0000000001
+    deviation = 0.01
     if (a_pushes < 0 or b_pushes < 0 or not is_whole(a_pushes, deviation) or not is_whole(b_pushes, deviation)): return 0
-    else: return (int(a_pushes) * 3) + int(b_pushes)
+    else: return (int(round(a_pushes)) * 3) + int(round(b_pushes))
 
 def is_whole(value, deviation):
     rounded = round(value)
@@ -56,8 +48,12 @@ Button B: X+27, Y+71
 Prize: X=18641, Y=10279"""
 
 def test_part01_sample():
-    assert 480 == part01(sample)
+    assert 480 == solution(sample)
 
 def test_part01_input():
     with open("src/inputs/day13.txt", "r") as f:
-        assert 36571 == part01(f.read())
+        assert 36571 == solution(f.read())
+
+def test_part02_input():
+    with open("src/inputs/day13.txt", "r") as f:
+        assert 85527711500010 == solution(f.read(), 10000000000000)
