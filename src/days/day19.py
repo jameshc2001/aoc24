f92640f974d5c26ddd12993e5bdad3e238888068
jameshc2001 <jameshcrofts@gmail.com>
2024-12-30 15:08:34 +0000

    day19 part02

--- src/days/day19.py
+++ src/days/day19.py
@@ -4,17 +4,22 @@ def part01(input):
     towels, patterns = input.split("\n\n")
     towels = tuple(towels.split(", "))
     patterns = patterns.split("\n")
-    return len([pattern for pattern in patterns if can_make_pattern(towels, pattern)])
+    return len([pattern for pattern in patterns if ways_to_make_pattern(towels, pattern) > 0])
+
+def part02(input):
+    towels, patterns = get_towels_and_patterns(input)
+    return sum([ways_to_make_pattern(towels, pattern) for pattern in patterns])
 
 @cache
-def can_make_pattern(towels, pattern):
-    if (len(pattern) == 0): return True
+def ways_to_make_pattern(towels, pattern):
+    if (len(pattern) == 0): return 1
+    return sum([ways_to_make_pattern(towels, pattern[len(towel):]) for towel in towels if towel == pattern[:len(towel)]])
 
-    for towel in towels:
-        if (len(towel) > len(pattern)): continue
-        if (towel == pattern[:len(towel)] and can_make_pattern(towels, pattern[len(towel):])): return True
-        
-    return False
+def get_towels_and_patterns(input):
+    towels, patterns = input.split("\n\n")
+    towels = tuple(towels.split(", "))
+    patterns = patterns.split("\n")
+    return towels,patterns
 
 #TESTS
 
@@ -35,3 +40,10 @@ def test_part01_sample():
 def test_part01_input():
     with open("src/inputs/day19.txt", "r") as f:
         assert 216 == part01(f.read())
+
+def test_part02_sample():
+    assert 16 == part02(sample)
+
+def test_part02_input():
+    with open("src/inputs/day19.txt", "r") as f:
+        assert 603191454138773 == part02(f.read())
