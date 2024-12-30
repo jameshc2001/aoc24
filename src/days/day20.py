12ecfb753e4b120985a9ab085351e74bb5f0413a
jameshc2001 <jameshcrofts@gmail.com>
2024-12-30 16:45:35 +0000

    day20 part01

--- src/days/day20.py
+++ src/days/day20.py
@@ -1,4 +1,5 @@
 import sys
+import heapq as hq
 
 def part01(input, picos_to_save):
 
@@ -16,41 +17,48 @@ def part01(input, picos_to_save):
     cheats = 0
     current_pos = start
     while (current_pos in prev):
-        #TODO: fancy cheat checking code here
+        for cheat_destination in get_adjacent(current_pos, 2):
+            if (cheat_destination not in positions): continue
+            time_saved = dist[current_pos] - dist[cheat_destination] - 2
+            if (time_saved >= picos_to_save): cheats += 1
 
         current_pos = prev[current_pos]
     assert current_pos == end
 
     return cheats
 
+#this is overkill and unnecessary
 def dijkstra(positions, start):
     dist = {}
     prev = {}
-    Q = set()
-    for pos in positions:
-        dist[pos] = sys.maxsize
-        Q.add(pos)
+    Q = []
+    removed = set()
     dist[start] = 0
+    for pos in positions:
+        if (pos != start): dist[pos] = sys.maxsize
+        hq.heappush(Q, (dist[pos], pos))
 
     while (len(Q) > 0):
-        u = min(Q, key=lambda pos: dist[pos])
-        Q.remove(u)
+        popped = hq.heappop(Q)
+        if (popped in removed): continue
+        u = popped[1]
 
         for v in get_adjacent(u):
             if (v not in positions): continue
             alt = dist[u] + 1
             if (alt < dist[v]):
+                removed.add((dist[v], v))
                 dist[v] = alt
                 prev[v] = u
+                hq.heappush(Q, (dist[v], v))
     
     return dist, prev
 
-
-def get_adjacent(pos): return [
-    (pos[0] + 1, pos[1]),
-    (pos[0], pos[1] + 1),
-    (pos[0] - 1, pos[1]),
-    (pos[0], pos[1] - 1),
+def get_adjacent(pos, offset=1): return [
+    (pos[0] + offset, pos[1]),
+    (pos[0], pos[1] + offset),
+    (pos[0] - offset, pos[1]),
+    (pos[0], pos[1] - offset),
 ]
 
 
@@ -74,3 +82,7 @@ sample = """###############
 
 def test_part01_sample():
     assert 8 == part01(sample, 12)
+
+def test_part01_input():
+    with open("src/inputs/day20.txt", "r") as f:
+        assert 1323 == part01(f.read(), 100)
