72c86ebd581ecbd5f2eaec3c145a05f833fb5723
jameshc2001 <jameshcrofts@gmail.com>
2024-12-30 11:24:38 +0000

    day18 part02

--- src/days/day18.py
+++ src/days/day18.py
@@ -3,36 +3,42 @@ import sys
 import heapq as hq
 
 def part01(input, num_of_bytes, end):
-    lines = input.split("\n")
-    corrupted = set()
-    for line_index in range(num_of_bytes):
-        nums = re.findall(r"\d+", lines[line_index])
-        corrupted.add((int(nums[0]), int(nums[1])))
+    corrupted, positions = get_initial_parameters(input, end)
+    return length_of_shortest_path(positions, set(corrupted[:num_of_bytes]), end)
 
-    positions = set()
-    for x in range(end + 1):
-        for y in range(end + 1):
-            if ((x, y) not in corrupted): positions.add((x, y))
+def part02(input, start_num_of_bytes, end):
+    corrupted, positions = get_initial_parameters(input, end)
+    
+    #binary search
+    left = start_num_of_bytes
+    right = len(corrupted)
+    midpoint = None
+    while (True):
+        midpoint = left + ((right - left) // 2)
+        if (left == midpoint or right == midpoint): break
+        shortest = length_of_shortest_path(positions, corrupted[:midpoint], end)
+        if (shortest == sys.maxsize):
+            right = midpoint
+        else:
+            left = midpoint
 
-    print_grid(positions, end)
-    dist, _ = dijkstra(positions, (0, 0))
+    first_blocking_byte = None
+    if (midpoint == left): first_blocking_byte = corrupted[right-1]
+    else: first_blocking_byte = corrupted[left-1]
+
+    return str(first_blocking_byte[0]) + "," + str(first_blocking_byte[1])
+
+def length_of_shortest_path(positions, corrupted, end):
+    dist, _ = dijkstra(positions, corrupted, (0, 0))
     return dist[(end, end)]
 
-def print_grid(positions, end):
-    output = "\n"
-    for y in range(end + 1):
-        for x in range(end + 1):
-            if (x, y) in positions: output += '.'
-            else: output += '#'
-        output += "\n"
-    print(output)
-
-def dijkstra(positions, start):
+def dijkstra(positions, corrupted, start):
     dist = {}
     prev = {}
     Q = []
     removed = set()
     for pos in positions:
+        if (pos in corrupted): continue
         dist[pos] = sys.maxsize
         hq.heappush(Q, (dist[pos], pos))
     dist[start] = 0
@@ -44,7 +50,7 @@ def dijkstra(positions, start):
         u = popped[1]
 
         for v in get_adjacent(u):
-            if (v not in positions): continue
+            if (v not in positions or v in corrupted): continue
             alt = dist[u] + 1
             if (alt < dist[v]):
                 removed.add((dist[v], v))
@@ -60,6 +66,19 @@ def get_adjacent(pos): return [
     (pos[0] - 1, pos[1]),
     (pos[0], pos[1] - 1)
 ]
+
+def get_initial_parameters(input, end):
+    lines = input.split("\n")
+    corrupted = []
+    for line in lines:
+        nums = re.findall(r"\d+", line)
+        corrupted.append((int(nums[0]), int(nums[1])))
+
+    positions = set()
+    for x in range(end + 1):
+        for y in range(end + 1):
+            positions.add((x, y))
+    return corrupted, positions
 
 #TESTS
 
@@ -95,3 +114,10 @@ def test_part01_sample():
 def test_part01_input():
     with open("src/inputs/day18.txt", "r") as f:
         assert 292 == part01(f.read(), 1024, 70)
+
+def test_part02_sample():
+    assert '6,1' == part02(sample, 12, 6)
+
+def test_part02_input():
+    with open("src/inputs/day18.txt", "r") as f:
+        assert '58,44' == part02(f.read(), 1024, 70)
