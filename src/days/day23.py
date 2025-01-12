c5c6f5a1212057e2da9e677d4f9eaaede8086293
jameshc2001 <jameshcrofts@gmail.com>
2025-01-12 14:59:44 +0000

    day23 part02

--- src/days/day23.py
+++ src/days/day23.py
@@ -1,14 +1,9 @@
 from collections import defaultdict
 from itertools import combinations
+from functools import cache
 
 def part01(input):
-    graph = defaultdict(set)
-    for line in input.split("\n"):
-        left, right = line.split("-")
-        graph[left].add(right)
-        graph[right].add(left)
-    graph = dict(graph)
-    
+    graph = get_graph(input)
     three_cliques = set()
     for node, neighbours in graph.items():
         if (node[0] != 't'): continue
@@ -19,6 +14,44 @@ def part01(input):
                 three_cliques.add(frozenset([node, a, b]))
 
     return len(three_cliques)
+
+
+def part02(input):
+    graph = get_graph(input)
+
+    @cache
+    def largest_clique_for_node(node, current_clique=frozenset()):
+        if (len(current_clique) > 0):
+            for other in current_clique:
+                if (node not in graph[other]): return current_clique
+
+        new_clique = frozenset(list(current_clique) + [node])
+        largest = new_clique
+        for other in graph[node]:
+            if (other in new_clique): continue
+            
+            maybe_largest = largest_clique_for_node(other, new_clique)
+            if (len(maybe_largest) > len(largest)):
+                largest = maybe_largest
+        
+        return largest
+
+    largest = frozenset()
+    for node in graph.keys():
+        maybe_largest = largest_clique_for_node(node)
+        if (len(maybe_largest) > len(largest)):
+            largest = maybe_largest
+    
+    return ','.join(sorted(largest))
+
+def get_graph(input):
+    graph = defaultdict(set)
+    for line in input.split("\n"):
+        left, right = line.split("-")
+        graph[left].add(right)
+        graph[right].add(left)
+    graph = dict(graph)
+    return graph
 
 #TESTS
 
@@ -61,3 +94,10 @@ def test_part01_sample():
 def test_part01_input():
     with open("src/inputs/day23.txt", "r") as f:
         assert 1512 == part01(f.read())
+
+def test_part02_sample():
+    assert "co,de,ka,ta" == part02(sample)
+
+def test_part02_input():
+    with open("src/inputs/day23.txt", "r") as f:
+        assert "ac,ed,fh,kd,lf,mb,om,pe,qt,uo,uy,vr,wg" == part02(f.read())
